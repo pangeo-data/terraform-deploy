c7b5fc3a65eb0419122bcd502d357f898878bf38
Sebastian Alvis <sebialvis@gmail.com>
2020-02-11 16:32:06 -0800

    Delete duplicate resources

--- aws/autoscaler.tf
+++ aws/autoscaler.tf
@@ -2,19 +2,6 @@ terraform {
   required_version = ">= 0.12.6"
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
-data "helm_repository" "stable" {
-  name = "stable"
-  url = "https://kubernetes-charts.storage.googleapis.com"
-}
-
 resource "helm_release" "cluster-autoscaler" {
   name = "cluster-autoscaler"
   # Check that this is good, kube-system should already exist
