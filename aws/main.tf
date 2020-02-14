f19373193b0215341398c2d0671f4b6dc6377f0a
YuviPanda <yuvipanda@gmail.com>
2020-02-14 09:39:21 -0800

    Remove automatic hub creation from here
    That should be done by hubploy

--- aws/main.tf
+++ aws/main.tf
@@ -166,3 +166,12 @@ module "eks" {
   map_users    = var.map_users
   map_accounts = var.map_accounts
 }
+
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
