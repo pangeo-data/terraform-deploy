eaaa9905efb0f5b65b7eb1247625bc005aaf9bb7
Sebastian Alvis <sebialvis@gmail.com>
2020-02-18 13:35:52 -0800

    Add helm provider block

--- monitoring/main.tf
+++ monitoring/main.tf
@@ -25,6 +25,14 @@ provider "kubernetes" {
   version                = "~> 1.11.0"
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
 data "helm_repository" "stable" {
   name = "stable"
   url = "https://kubernetes-charts.storage.googleapis.com"
