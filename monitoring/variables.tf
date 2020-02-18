1b23c23c5e2372ec9933535fc8e4656c4ad47852
Sebastian Alvis <sebialvis@gmail.com>
2020-02-18 15:57:40 -0800

    Added variable descriptions

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -1,11 +1,14 @@
 variable "profile" {
-	default = "terraform-bot"
+	default     = "terraform-bot"
+  description = "the profile that the awscli will use to run commands."
 }
 
 variable "region" {
-	default = "us-east-1"
+	default     = "us-east-1"
+  description = "The availability region where your cluster is."
 }
 
 variable "cluster_name" {
-  default = "eks-cluster-example-name"
+  default     = "eks-cluster-example-name"
+  description = "The name of the existing cluster."
 }
