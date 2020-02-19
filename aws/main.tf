615d32165db397e663b9812188a8fe151fda1197
Sebastian Alvis <sebialvis@gmail.com>
2020-02-18 16:00:30 -0800

    Fix helm provider release at 1.0
    This release supports helm 3.

--- aws/main.tf
+++ aws/main.tf
@@ -112,6 +112,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
+  version = "~>1.0.0"
 }
 
 module "monitoring" {
--- monitoring/main.tf
+++ monitoring/main.tf
@@ -31,6 +31,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
+  version = "~>1.0.0"
 }
 
 data "helm_repository" "stable" {
