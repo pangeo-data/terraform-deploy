13efec05916a496340eb26aa436401950dea9817
Sebastian Alvis <sebialvis@gmail.com>
2020-07-27 14:01:39 -0700

    Added fs ID note, whitespace

--- aws/mount_volume.sh
+++ aws/mount_volume.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 echo "Running start-up script as root"
+
 # Auto-Mount EFS Drive  
 yum install -y amazon-efs-utils
 MOUNTPOINT=/mnt/efs
 mkdir $MOUNTPOINT
+
+# NEED TO CHANGE FS ID
 sudo mount -t efs fs-382b4792:/ $MOUNTPOINT
 echo "Ephemeral disk mounted to $MOUNTPOINT"
