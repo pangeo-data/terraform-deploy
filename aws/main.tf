f8e47dba242e7b77c4f8b380deb2462700ea743d
Sebastian Alvis <sebialvis@gmail.com>
2020-09-01 15:22:23 -0700

    Remove old merge characters, extra data block

--- aws/main.tf
+++ aws/main.tf
@@ -21,8 +21,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-data "aws_caller_identity" "current" {}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
@@ -109,8 +107,6 @@ module "eks" {
      }
     }
   }
-
->>>>>>> df212acf42c77d0c87a1e52b35db733d78a7af7d
 
   map_accounts = var.map_accounts
   map_users = var.map_users
