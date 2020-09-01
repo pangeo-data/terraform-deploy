35794a14a14f836c0d56ad320b4dcefd291c8d84
Sebastian Alvis <sebialvis@gmail.com>
2020-09-01 16:57:54 -0700

    Add ami search, plug into ec2 instance block

--- aws-examples/minimal-deployment-tutorial/ec2-intro-tutorial/main.tf
+++ aws-examples/minimal-deployment-tutorial/ec2-intro-tutorial/main.tf
@@ -8,20 +8,29 @@ provider "aws" {
   profile = var.profile
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon-linux" {
   most_recent      = true
-  owners           = ["self"]
-  executable_users = ["self"]
+  owners           = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.040amd64-server-*"]
+    values = ["amzn-ami*2018.03.0.2*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
   }
 }
 
 resource "aws_instance" "test-ec2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.amazon-linux.id
+  instance_type = "t2.micro"
 
   tags = {
     Name = "${var.deployment_name}"
