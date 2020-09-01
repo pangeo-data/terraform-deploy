3d2a737adf516eeb046456c30fc6594e53bf1005
Sebastian Alvis <sebialvis@gmail.com>
2020-09-01 16:58:01 -0700

    Add outputs

---
+++ aws-examples/minimal-deployment-tutorial/ec2-intro-tutorial/outputs.tf
@@ -0,0 +1,9 @@
+output "ami-id" {
+  description = "ID of the Amazon Machine Image used for our EC2 instance"
+  value = "${data.aws_ami.amazon-linux.id}"
+}
+
+output "name" {
+  description = "Amazon Resource Name of our EC2 instance"
+  value = "${aws_instance.test-ec2.arn}"
+}
