b7b01cecb781add5e4584818a87298862c293df5
cslocum <slocum.christine@gmail.com>
2020-09-29 21:22:20 +0000

    more progress on consolidating roles

---
+++ aws/roman-sit.tfvars
@@ -0,0 +1,7 @@
+# Put your cluster where your data is
+region = "us-east-1"
+
+# Name of your cluster
+cluster_name = "roman-sit"
+
+allowed_roles = ["arn:aws:iam::328656936502:role/jupyterhub-admin"]
---
+++ kms-codecommit/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "repo_name" {
+  default = "<change-this-in-tfvars>"
+}
+
+variable "sops_s3_bucket" {
+  default = "<change-this-in-tfvars>"
+}
+
+variable "sops_s3_key" {
+  default = "<change-this-in-tfvars>"
+}
+
+variable "sops_s3_source" {
+  default = "<change-this-in-tfvars>"
+}
+
+variable "owner_tag" {
+  default = ""
+}
