e662f7e6cbaa593f996122b68a6b42bf034b45cc
Todd Miller <jmiller@stsci.edu>
2020-10-02 21:31:50 +0000

    Added local.private_subnet_cidrs to EFS as replacement for vpc_cidr.

--- aws/efs.tf
+++ aws/efs.tf
@@ -11,6 +11,7 @@ resource "aws_security_group" "home_dirs_sg" {
 
   # NFS
   ingress {
+    cidr_blocks = local.private_subnet_cidrs
     security_groups = [ module.eks.worker_security_group_id ]
     from_port        = 2049
     to_port          = 2049
