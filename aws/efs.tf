71ed1873b4afa2c2532f3c090a8673e1dd0ca4d4
TheRealZoidberg <60232355+TheRealZoidberg@users.noreply.github.com>
2021-01-14 09:37:20 -0500

    Update efs.tf
    removed the following as its not needed
    data "helm_repository" "stable" {
      name = "stable"
      #url = "https://kubernetes-charts.storage.googleapis.com"
      url = "https://charts.helm.sh/stable"
    }

--- aws/efs.tf
+++ aws/efs.tf
@@ -30,12 +30,6 @@ resource "aws_efs_mount_target" "home_dirs_targets" {
   security_groups = [ aws_security_group.home_dirs_sg.id ]
 }
 
-data "helm_repository" "stable" {
-  name = "stable"
-  #url = "https://kubernetes-charts.storage.googleapis.com"
-  url = "https://charts.helm.sh/stable"
-}
-
 resource "kubernetes_namespace" "support" {
   metadata {
     name = "support"
